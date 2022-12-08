b3c37d6e7c3b601547ad435ac80881146bbada10
Abhimanyu Singh <abhimanyu.new@gmail.com>
2022-12-08 08:58:23 +0000

    added FAQ

--- src/data/static/site-settings.ts
+++ src/data/static/site-settings.ts
@@ -5,6 +5,7 @@ import promoImg1 from '@/assets/images/promo/1.png';
 import promoImg3 from '@/assets/images/promo/3.png';
 import promoImg4 from '@/assets/images/promo/4.png';
 import promoImg5 from '@/assets/images/promo/5.png';
+import christmas from '@/assets/images/promo/christmas.png';
 
 export const siteSettings = {
   lightLogo:
@@ -12,6 +13,7 @@ export const siteSettings = {
   darkLogo:
     'https://uploads-ssl.webflow.com/620ad29fb46ac6596379e3a5/620d0dde8482383154cce5b6_Less%20Background%20Logo.png',
   promoCarouselImages: [
+    christmas,
     promoImgFoodHak,
     promoImg3,
     promoImg4,
@@ -19,6 +21,7 @@ export const siteSettings = {
     promoImg5,
   ],
   promoCarouselLinks: [
+    'https://www.marvinsden.com/faq', // FAQs
     '/preppers/RES1666108454SIX33686', // Foodhak
     '/preppers/RES1663343678TUT42260', //Field Doctor
     'https://qbs4or3rcm8.typeform.com/to/jtWs4On7',
