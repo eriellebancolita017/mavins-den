c800b0cbdef672369669b9e82d5512bc5b5b1750
abhi16 <abhimanyu.new@gmail.com>
2022-12-13 07:27:20 +0000

    Added hotjar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -260,6 +260,22 @@ function CustomApp({ Component, pageProps }: AppPropsWithLayout) {
           `,
         }}
       />
+      <Script
+        id="hotjar"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:3286137,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+        }}
+      />
       <Hydrate state={pageProps.dehydratedState}>
         <ThemeProvider
           attribute="class"
