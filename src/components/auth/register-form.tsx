6eb449736255e0f9e772c2fb7a39335b6fbcc3df
abhi16 <abhimanyu.new@gmail.com>
2023-01-08 13:16:33 +0000

    Disable OTP

--- src/components/auth/register-form.tsx
+++ src/components/auth/register-form.tsx
@@ -41,13 +41,9 @@ export default function RegisterUserForm() {
   const { authorize } = useAuth();
   const { location, setUserInfo } = useUserContext();
   const { phoneNumber } = usePhoneInput();
-  const [codeSent, setCodeSent] = useState(false);
-  const [time, setTime] = useState(0);
-  const timerRef = useRef<any>();
-  const timeTempRef = useRef<number>(0);
 
   let [serverError, setServerError] = useState<RegisterUserInput | null>(null);
-  const { mutate, isLoading } = useMutation(client.users.register, {
+  const { mutate } = useMutation(client.users.register, {
     onSuccess: (res) => {
       if (!res.payload.consumer_id) {
         toast.error(<b>Something went wrong</b>, {
@@ -82,59 +78,19 @@ export default function RegisterUserForm() {
     },
   });
 
-  const { mutate: optMutate, isLoading: optLoading } = useMutation(
-    client.users.getOpt,
-    {
-      onSuccess: (res) => {
-        if (!res.payload.otp) {
-          toast.error(<b>Something went wrong</b>, {
-            className: '-mt-10 xs:mt-0',
-          });
-          return;
-        }
-        toast.success(<b>Code Sent!</b>, {
-          className: '-mt-10 xs:mt-0',
-        });
-        clearTimeout(timerRef.current);
-        setCodeSent(true);
-        setTime(0);
-        timeTempRef.current = 0;
-        timerRef.current = setInterval(() => {
-          timeTempRef.current++;
-          setTime(timeTempRef.current);
-
-          if (timeTempRef.current === 60) {
-            clearTimeout(timerRef.current);
-            setCodeSent(false);
-          }
-        }, 1000);
-      },
-    }
-  );
   const onSubmit: SubmitHandler<RegisterUserInput> = (data) => {
-    clearTimeout(timerRef.current);
     mutate({
       ...data,
-
       register_type: 'email',
       mobile_country_code: '+44',
       code: 'EN',
-      device_id: 'xxx',
-      device_token: 'xxx',
+      device_id: 'web-shop',
+      device_token: 'web-shop',
       device_type: 'android',
-      device_name: 'xxx',
+      device_name: 'web-shop',
       user_type: 'consumer',
       mobile: phoneNumber.substring(2),
-    });
-  };
-
-  const codeClick = () => {
-    optMutate({
-      type: 'consumer',
-      mobile: phoneNumber.substring(2),
-      mobile_country_code: '+44',
-      request_type: 'signUp',
-      code: 'EN',
+      otp: 9999,
     });
   };
 
@@ -160,7 +116,6 @@ export default function RegisterUserForm() {
 
           <Form<RegisterUserInput>
             onSubmit={onSubmit}
-            validationSchema={registerUserValidationSchema}
             serverError={
               serverError as
                 | ServerErrors<Partial<RegisterUserInput>>
@@ -189,27 +144,9 @@ export default function RegisterUserForm() {
                         : 'Please input correct phone number'}
                     </span>
                   ) : (
-                    <p
-                      onClick={() => codeClick()}
-                      className="float-right mt-2 cursor-pointer text-sm font-semibold text-brand hover:text-brand-dark"
-                    >
-                      {!codeSent ? 'Send code' : 'Re-send code'}
-                    </p>
+                    <p></p>
                   )}
                 </div>
-                <Input
-                  label="Verify your code"
-                  inputClassName="bg-light dark:bg-dark-300"
-                  type="number"
-                  {...register('otp')}
-                  error={errors.otp ? 'Code should be 4 digits' : ''}
-                  disabled={optLoading || !codeSent}
-                />
-                {codeSent && (
-                  <p className="float-right -translate-y-4 text-warning">{`00 : ${
-                    60 - time >= 10 ? 60 - time : '0' + (60 - time)
-                  }`}</p>
-                )}
                 <Input
                   label="Email"
                   inputClassName="bg-light dark:bg-dark-300"
@@ -226,7 +163,7 @@ export default function RegisterUserForm() {
                 <Button
                   type="submit"
                   className="!mt-5 w-full text-sm tracking-[0.2px] lg:!mt-7"
-                  disabled={isLoading}
+                  disabled={false}
                 >
                   Register
                 </Button>
